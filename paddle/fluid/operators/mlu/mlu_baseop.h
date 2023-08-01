203d9686d5248cea8fc297747463db5d75e8f926
liupeiyu <liupeiyu@cambricon.com>
2023-08-01 14:17:37 +0800

    [MLU] fix bn and enable tf32

--- paddle/fluid/operators/mlu/mlu_baseop.h
+++ paddle/fluid/operators/mlu/mlu_baseop.h
@@ -29,8 +29,8 @@ limitations under the License. */
 namespace paddle {
 namespace operators {
 
-using Tensor = framework::Tensor;
-using DataLayout = framework::DataLayout;
+using Tensor = phi::DenseTensor;
+using DataLayout = phi::DataLayout;
 using ExecutionContext = framework::ExecutionContext;
 using DeviceContextPool = platform::DeviceContextPool;
 using MLUDeviceContext = platform::MLUDeviceContext;
@@ -87,9 +87,9 @@ inline cnnlInterpBackwardMode_t GetMLUCnnlInterpBackwardMode(
       "Not support interp mode of MLU Device: %s", interp_mode));
 }
 
-inline const void* GetBasePtr(const Tensor* t) { return t->data(); }
+inline const void* GetBasePtr(const phi::DenseTensor* t) { return t->data(); }
 
-inline void* GetBasePtr(Tensor* t) { return t->data(); }
+inline void* GetBasePtr(phi::DenseTensor* t) { return t->data(); }
 
 inline cnnlDataType_t ToCnnlDataType(
     const paddle::experimental::DataType& dtype) {
@@ -309,18 +309,18 @@ class MLUCnnlTensorDesc {
                     const cnnlDataType_t tensor_dtype,
                     int position);
 
-  MLUCnnlTensorDesc(const Tensor& tensor,
+  MLUCnnlTensorDesc(const phi::DenseTensor& tensor,
                     const cnnlTensorLayout_t layout,
                     const cnnlDataType_t tensor_dtype);
 
-  explicit MLUCnnlTensorDesc(const Tensor& tensor);
+  explicit MLUCnnlTensorDesc(const phi::DenseTensor& tensor);
 
-  MLUCnnlTensorDesc(const Tensor& tensor,
+  MLUCnnlTensorDesc(const phi::DenseTensor& tensor,
                     cnnlTensorLayout_t layout,
                     const cnnlDataType_t tensor_dtype,
                     int position);
 
-  MLUCnnlTensorDesc(const Tensor& tensor,
+  MLUCnnlTensorDesc(const phi::DenseTensor& tensor,
                     cnnlTensorLayout_t layout,
                     const cnnlDataType_t tensor_dtype,
                     int position,
@@ -406,7 +406,9 @@ class MLUCnnlActivationDesc {
  public:
   MLUCnnlActivationDesc(const MLUCnnlActivationDesc& desc) = delete;
   MLUCnnlActivationDesc& operator=(const MLUCnnlActivationDesc& desc) = delete;
-  MLUCnnlActivationDesc(const cnnlActivationMode_t act_mode, const float ceof);
+  MLUCnnlActivationDesc(const cnnlActivationMode_t act_mode,
+                        const float ceof,
+                        const int sliced_dim = 1);
   MLUCnnlActivationDesc(const cnnlActivationMode_t act_mode,
                         const float ceof,
                         const float sliced_dim,
@@ -1203,6 +1205,7 @@ class MLUCnnl {
   static void BatchMatmul(const ExecutionContext& ctx,
                           const bool transpose_a,
                           const bool transpose_b,
+                          const cnnlDataType_t data_type,
                           const cnnlTensorDescriptor_t in0_desc,
                           const void* in0,
                           const cnnlTensorDescriptor_t in1_desc,
@@ -1750,6 +1753,9 @@ class MLUCnnl {
 
   static void FusedBatchNorm(const ExecutionContext& ctx,
                              const bool is_training,
+                             const cnnlActivationDescriptor_t activation_desc,
+                             const cnnlBatchNormMode_t mode,
+                             const cnnlBatchNormOps_t bnOps,
                              const cnnlTensorDescriptor_t x_desc,
                              const void* x,
                              const cnnlTensorDescriptor_t scale_desc,
@@ -1766,21 +1772,25 @@ class MLUCnnl {
                              void* saved_mean,
                              void* saved_var);
 
-  static void FusedBatchNormGrad(const ExecutionContext& ctx,
-                                 const bool is_training,
-                                 const cnnlTensorDescriptor_t y_backprop_desc,
-                                 const void* y_backprop,
-                                 const cnnlTensorDescriptor_t x_desc,
-                                 const void* x,
-                                 const cnnlTensorDescriptor_t scale_desc,
-                                 const void* scale,
-                                 const void* saved_mean,
-                                 const void* saved_var,
-                                 float epsilon,
-                                 const cnnlTensorDescriptor_t x_backprop_desc,
-                                 void* x_backprop,
-                                 void* scale_backprop,
-                                 void* offset_backprop);
+  static void FusedBatchNormGrad(
+      const ExecutionContext& ctx,
+      const bool is_training,
+      const cnnlActivationDescriptor_t activation_desc,
+      const cnnlBatchNormMode_t mode,
+      const cnnlBatchNormOps_t bnOps,
+      const cnnlTensorDescriptor_t y_backprop_desc,
+      const void* y_backprop,
+      const cnnlTensorDescriptor_t x_desc,
+      const void* x,
+      const cnnlTensorDescriptor_t scale_desc,
+      const void* scale,
+      const void* saved_mean,
+      const void* saved_var,
+      float epsilon,
+      const cnnlTensorDescriptor_t x_backprop_desc,
+      void* x_backprop,
+      void* scale_backprop,
+      void* offset_backprop);
 
   static void LayerNormForward(const ExecutionContext& ctx,
                                int axis,
@@ -2398,8 +2408,8 @@ inline void SetMLUTransposePerm(const framework::DDim& dims,
 template <typename T>
 inline void TransposeFromMLUTensor(const ExecutionContext& ctx,
                                    const std::vector<int> perm,
-                                   const Tensor* transformed_input,
-                                   Tensor* transformed_output,
+                                   const phi::DenseTensor* transformed_input,
+                                   phi::DenseTensor* transformed_output,
                                    bool need_reshape_or_alloc) {
   const int dim_size = perm.size();
   if (need_reshape_or_alloc) {
@@ -2428,7 +2438,7 @@ inline void TransposeFromMLUTensor(const ExecutionContext& ctx,
 template <typename T>
 inline void FillMLUTensorWithHostValue(const ExecutionContext& ctx,
                                        T value,
-                                       Tensor* out) {
+                                       phi::DenseTensor* out) {
   MLUCnnlTensorDesc out_desc(*out);
   MLUCnnl::Fill(
       ctx, CNNL_POINTER_MODE_HOST, &value, out_desc.get(), GetBasePtr(out));
